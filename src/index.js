bd32cba9004b15d3142901507df68ac462e402e6
GOLF <golf.prptmk@gmail.com>
2019-05-01 05:11:45 +0700

    +body

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import './index.css';
 class App extends React.Component {
     render() {
         return (<div className="app" >
-            <div class="grid-container" >
+            <div>
                 <nav class="nav-div" >
                     <div class="col6" >
                         <div class="logo-div" >
                             <img src="static/img/logo.png" alt="" />
                         </div>
-                        <span class="web-title"> MASS RAPID TRANSIT </span>
+                        <span class="web-title"> BANGKOK EXPRESSWAY AND METRO </span>
                     </div>
                     <div class="col6 nav-item-div" >
                         <div > About </div>
@@ -20,20 +20,44 @@ class App extends React.Component {
                         <div > Contact </div>
                     </div>
                 </nav>
-                <div class="grid-header" >
-                    <div class="hero-div flex-middle">
-                        <div class="hero-text" >
-                            <span class="hero-title" > Most comfortable </span>
-                            <span class="hero-subtitle" > Rapid Transit System </span>
-                            <form class="display-flex big-searchbar">
-                                <input class="big-search" name="station" placeholder="Search MRT station" />
-                                <button class="big-search-btn" ><img src="static/ico/search-solid.svg" alt=""/></button>
-                            </form>
-                        </div>
+                <div class="hero-div flex-middle">
+                    <div class="hero-text" >
+                        <span class="hero-title" > Most comfortable </span>
+                        <span class="hero-subtitle" > Rapid Transit System </span>
+                        <form class="display-flex big-searchbar">
+                            <input class="big-search" name="station" placeholder="Search MRT station" />
+                            <button class="big-search-btn" ><img src="static/ico/search-solid.svg" alt="" /></button>
+                        </form>
                     </div>
                 </div>
-                <div class="grid-main">
-                
+                <div>
+                    <div class="section1 grid-container-quicklink">
+                        <div class="box link1">
+                            <span class="linkname-div">
+                                <span class="linkname-text">MRT Route Maps</span>
+                            </span>
+                        </div>
+                        <div class="box link2">
+                            <span class="linkname-div">
+                                <span class="linkname-text">Train Tickets</span>
+                            </span>
+                        </div>
+                        <div class="box link3">
+                            <span class="linkname-div">
+                                <span class="linkname-text">Contact MRT</span>
+                            </span>
+                        </div>
+                        <div class="box link4">
+                            <span class="linkname-div">
+                                <span class="linkname-text">Suggestion and services</span>
+                            </span>
+                        </div>
+                        <div class="box link5">
+                            <span class="linkname-div">
+                                <span class="linkname-text">Fare Calculation</span>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div >
